28bbc3054843ae74fbcacc4b91a257bcc8e870ce
valurp <vpv1@hi.is>
2021-03-26 14:25:14 +0000

    Removed unused floating action button

--- app/src/main/java/is/hi/hbv601g/gjaldbrotapp/NavigationActivity.java
+++ app/src/main/java/is/hi/hbv601g/gjaldbrotapp/NavigationActivity.java
@@ -24,14 +24,6 @@ public class NavigationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_navigation);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
