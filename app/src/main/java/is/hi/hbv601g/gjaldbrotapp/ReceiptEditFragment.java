f7d584bb6047e938205473d375de433c72e69da0
jaykaytherobot <johannekarisolmundarson@gmail.com>
2021-04-14 10:15:43 +0000

    okei, veit ekki afhverju eg leitadi ekki strax ad datePicker i android

--- app/src/main/java/is/hi/hbv601g/gjaldbrotapp/ReceiptEditFragment.java
+++ app/src/main/java/is/hi/hbv601g/gjaldbrotapp/ReceiptEditFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -32,23 +33,23 @@ import is.hi.hbv601g.gjaldbrotapp.Services.ReceiptService;
 public class ReceiptEditFragment extends Fragment {
     private static String TAG = "DATE_PICKER";
 
-    private Spinner mYearSpinner;
-    private Spinner mMonthSpinner;
-    private Spinner mDaySpinner;
-    private int day = 1; // þessi breyta er til að uppfæra daySpinner eftir að monthSpinner hefur verið valinn
+    private DatePicker mDatePicker;
 
     private EditText mAmountField;
     private EditText mTimeField;
     private Spinner mTypeSpinner;
+    private List<Type> mTypeList;
 
     private ReceiptItem mReceiptItem;
 
     public ReceiptEditFragment() {
         mReceiptItem = new ReceiptItem();
+        mTypeList = new ArrayList<Type>();
     }
 
     public ReceiptEditFragment(ReceiptItem receiptItem) {
         mReceiptItem = receiptItem;
+        mTypeList = new ArrayList<Type>();
     }
 
     @Override
@@ -64,20 +65,8 @@ public class ReceiptEditFragment extends Fragment {
         /**
          * Setja upp dagsetningar spinner-a
          */
-        mYearSpinner = (Spinner) view.findViewById(R.id.receipt_edit_sp_year);
-        mMonthSpinner = (Spinner) view.findViewById(R.id.receipt_edit_sp_month);
-        mDaySpinner = (Spinner) view.findViewById(R.id.receipt_edit_sp_day);
+        mDatePicker = (DatePicker) view.findViewById(R.id.edit_receipt_datepicker);
 
-        ArrayAdapter<CharSequence> yearAdapter = ArrayAdapter.createFromResource(this.getContext(),
-                R.array.years, android.R.layout.simple_spinner_item);
-        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mYearSpinner.setAdapter(yearAdapter);
-
-        ArrayAdapter<CharSequence> monthAdapter = ArrayAdapter.createFromResource(this.getContext(),
-                R.array.months, android.R.layout.simple_spinner_item);
-        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mMonthSpinner.setAdapter(monthAdapter);
-        mMonthSpinner.setOnItemSelectedListener(new OnMonthSelected());
 
         /**
          * Tengja rest af inputum
@@ -101,19 +90,14 @@ public class ReceiptEditFragment extends Fragment {
     }
 
     private void initializeForm() {
-        if (mReceiptItem == null) {
-            setDate(new Date());
-            return;
-        }
-        Log.i(TAG, mReceiptItem.getDate().toString());
         setDate(mReceiptItem.getDate());
-        mAmountField.setText(mReceiptItem.getAmount()+"");
+        mAmountField.setText(String.format("%d", mReceiptItem.getAmount()));
     }
 
     private String getDateSelected() {
-        String year = mYearSpinner.getSelectedItem().toString();
-        String month = mMonthSpinner.getSelectedItemPosition()+1+"";
-        String day = mDaySpinner.getSelectedItem().toString();
+        String year = String.valueOf(mDatePicker.getYear());
+        String month = String.valueOf(mDatePicker.getMonth());
+        String day = String.valueOf(mDatePicker.getDayOfMonth());
         Log.i(TAG, String.format("%s-%s-%s", year, month, day));
         return String.format("%s-%s-%s", year, month, day);
     }
@@ -124,11 +108,7 @@ public class ReceiptEditFragment extends Fragment {
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
-        Log.i(TAG+" month", month+"");
-        Log.i(TAG+" day", day-1+"");
-        // TODO upphafsstilla árið í yearSpinner
-        this.day = day-1;
-        mMonthSpinner.setSelection(month);
+        mDatePicker.updateDate(year, month, day);
     }
 
     /**
@@ -140,7 +120,7 @@ public class ReceiptEditFragment extends Fragment {
         String date = getDateSelected();
         String amount = mAmountField.getText().toString();
         String time = mTimeField.getText().toString();
-        String type = mTypeSpinner.getSelectedItem().toString();
+        String type = getIdOfTypeSelected();
 
         if (date.equals("") || amount.equals("") || time.equals("") || type.equals("")) {
             throw new Exception("Input error, not all fields are filled in");
@@ -157,6 +137,16 @@ public class ReceiptEditFragment extends Fragment {
         return mReceiptItem;
     }
 
+    private String getIdOfTypeSelected() {
+        String typeSelected = mTypeSpinner.getSelectedItem().toString();
+        for (Type t : mTypeList) {
+            if (t.getName().equals(typeSelected)) {
+                return String.format("%d", t.getId());
+            }
+        }
+        return "";
+    }
+
     private class FetchTypesTask extends AsyncTask<ReceiptItem, Void, List<Type>> {
         private ArrayAdapter mTypeAdapter;
         public FetchTypesTask(ArrayAdapter typeAdapter) {
@@ -171,42 +161,12 @@ public class ReceiptEditFragment extends Fragment {
         @Override
         public void onPostExecute(List<Type> result) {
             mTypeAdapter.clear();
+            mTypeList.clear();
             for (Type t : result) {
                 mTypeAdapter.add(t.getName());
+                mTypeList.add(t);
             }
             mTypeAdapter.notifyDataSetChanged();
-        }
-    }
-
-    private class OnMonthSelected implements AdapterView.OnItemSelectedListener {
-        @Override
-        public void onItemSelected(AdapterView parent, View view, int pos, long id) {
-            // TODO geyma hvaða item er selected og setja það eftir að breytt er um dagafjölda
-            // notar id af mánuði sem er valinn til að setja réttan dagafjölda í daySpinner
-            // allir mánuðir hafa amk 28 daga
-            List<Integer> days = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28));
-            List<Integer> thirtyOneDayMonths = new ArrayList<Integer>(Arrays.asList(0,2,4,6,7,9,11));
-            if(pos == 1) { // TODO factor in leap year for february
-                // do nothing if month is february
-            }
-            else { // all other month have atleast 30 days
-                days.add(29);
-                days.add(30);
-            }
-            if(thirtyOneDayMonths.contains(pos)) {
-                days.add(31);
-            }
-            ArrayAdapter<Integer> dayAdapter =
-                    new ArrayAdapter<Integer>(ReceiptEditFragment.this.getContext(),
-                            android.R.layout.simple_spinner_dropdown_item,
-                            days);
-            dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            mDaySpinner.setAdapter(dayAdapter);
-            mDaySpinner.setSelection(day);
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView parent) {
         }
     }
 }
