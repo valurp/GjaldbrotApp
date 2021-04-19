edc5f28116c0ad936449911b2acf59236cfa5f8f
jaykaytherobot <johannekarisolmundarson@gmail.com>
2021-04-19 17:10:25 +0000

    recycler view fyrir receiptTypes

--- app/src/main/java/is/hi/hbv601g/gjaldbrotapp/ui/monthly_overview/MonthlyOverviewFragment.java
+++ app/src/main/java/is/hi/hbv601g/gjaldbrotapp/ui/monthly_overview/MonthlyOverviewFragment.java
@@ -1,5 +1,7 @@
 package is.hi.hbv601g.gjaldbrotapp.ui.monthly_overview;
 
+import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -7,6 +9,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +23,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GridLabelRenderer;
@@ -23,11 +34,14 @@ import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 
+import org.w3c.dom.Text;
+
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import is.hi.hbv601g.gjaldbrotapp.Entities.OverviewGroup;
+import is.hi.hbv601g.gjaldbrotapp.Entities.Type;
 import is.hi.hbv601g.gjaldbrotapp.R;
 import is.hi.hbv601g.gjaldbrotapp.Services.ReceiptService;
 
@@ -35,6 +49,12 @@ public class MonthlyOverviewFragment extends Fragment {
     private String TAG = "MontlyOverview";
     private GraphView mGraph;
     private List<OverviewGroup> mOverviewData;
+    private TypeRecyclerAdapter mAdapter;
+
+    public MonthlyOverviewFragment() {
+        mOverviewData = new ArrayList<>();
+        mAdapter = new TypeRecyclerAdapter(mOverviewData);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
@@ -46,7 +66,7 @@ public class MonthlyOverviewFragment extends Fragment {
         mGraph.getGridLabelRenderer().setLabelFormatter(new LabelFormatter() {
             @Override
             public String formatLabel(double value, boolean isValueX) {
-                if (isValueX) return "matur";
+                if (isValueX) return "";
                 return (int) value + " kr";
             }
             @Override
@@ -55,6 +75,10 @@ public class MonthlyOverviewFragment extends Fragment {
             }
         });
         mGraph.getGridLabelRenderer().setNumHorizontalLabels(3);
+
+        RecyclerView legend = (RecyclerView) view.findViewById(R.id.overview_legend);
+        legend.setLayoutManager(new LinearLayoutManager(getContext()));
+        legend.setAdapter(mAdapter);
 
         new FetchOverviewTask().execute();
 
@@ -90,7 +114,8 @@ public class MonthlyOverviewFragment extends Fragment {
         @Override
         public void onPostExecute(List<OverviewGroup> result) {
             Log.i(TAG, "fetched data");
-            mOverviewData = result;
+            mOverviewData.addAll(result);
+            mAdapter.notifyDataSetChanged();
             updateGraph();
         }
     }
@@ -119,6 +144,38 @@ public class MonthlyOverviewFragment extends Fragment {
 
         public int getColor() {
             return mColor;
+        }
+    }
+
+    private class TypeRecyclerAdapter extends RecyclerView.Adapter<TypeRecyclerAdapter.ViewHolder>{
+        private List<OverviewGroup> mValues;
+
+        public TypeRecyclerAdapter(List<OverviewGroup> values) {
+            mValues = values;
+        }
+
+        @Override
+        public TypeRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.type_layout, parent, false);
+            return new TypeRecyclerAdapter.ViewHolder(view);
+        }
+
+        public void onBindViewHolder(final TypeRecyclerAdapter.ViewHolder holder, int position) {
+            holder.mTextView.setText(mValues.get(position).getCategory());
+            holder.mColor.setBackgroundColor(Color.rgb(200,100,100));
+        }
+
+        public int getItemCount() { return mValues.size(); }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView mColor;
+            TextView mTextView;
+            public ViewHolder(View view) {
+                super(view);
+                mTextView = (TextView) view.findViewById(R.id.overview_legend_title);
+                mColor = (TextView) view.findViewById(R.id.overview_legend_color);
+            }
         }
     }
 }
